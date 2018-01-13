a1a467b82c2fa135da94cebfc71c79b1cf0ca0d0
Matthew Hutchinson <matt@hiddenloop.com>
2018-01-13 13:43:49 +0000

    refactor plugin config for lolcommits v0.10.0
    * require at least lolcommits >= 0.10.0
    * drop`self.name` method, plugins are now identified by their gem name
    * drop calls to `configured?` (no longer available)
    * symbolize all plugin config keys
    * update rubies for Travis CI
    * bump gem version

--- lib/lolcommits/plugin/hipchat.rb
+++ lib/lolcommits/plugin/hipchat.rb
@@ -1,18 +1,10 @@
 require 'mime/types'
+require 'net/http'
 require 'lolcommits/plugin/base'
 
 module Lolcommits
   module Plugin
     class Hipchat < Base
-
-      ##
-      # Returns the name of this plugin.
-      #
-      # @return [String] the plugin name
-      #
-      def self.name
-        'hipchat'
-      end
 
       ##
       # Returns position(s) of when this plugin should run during the capture
@@ -25,19 +17,6 @@ module Lolcommits
       end
 
       ##
-      # Returns true if the plugin has been configured. An API token, team name
-      # and HipChat room id (or name) are required.
-      #
-      # @return [Boolean] true/false indicating if plugin is configured
-      #
-      def configured?
-        super &&
-          !!(configuration['api_token'] &&
-             configuration['api_team'] &&
-             configuration['api_room'])
-      end
-
-      ##
       # Returns true/false indicating if the plugin has been correctly
       # configured. To post a message to HipChat all plugin options must be
       # set.
@@ -46,7 +25,7 @@ module Lolcommits
       # configured
       #
       def valid_configuration?
-        %w(api_token api_team api_room).all? do |option|
+        [:api_token, :api_team, :api_room].all? do |option|
           !configuration[option].to_s.strip.empty?
         end
       end
@@ -60,7 +39,7 @@ module Lolcommits
       #
       def configure_options!
         options = super
-        options.merge!(configure_auth_options) if options['enabled']
+        options.merge!(configure_auth_options) if options[:enabled]
         options
       end
 
@@ -75,7 +54,7 @@ module Lolcommits
       # @return [Boolean] indicating if HipChat post was successful
       #
       def run_capture_ready
-        print "Posting to HipChat (#{configuration['api_room']}) ..."
+        print "Posting to HipChat (#{configuration[:api_room]}) ..."
         debug "Posting to HipChat: HTTP POST to #{api_url}"
 
         boundary = "0123456789ABLEWASIEREISAWELBA9876543210"
@@ -107,14 +86,14 @@ module Lolcommits
 
       def configure_auth_options
         puts "\n"
-        puts '-' * 50
+        puts '-' * 41
         puts ' Lolcommits HipChat Plugin Configuration'
-        puts '-' * 50
+        puts '-' * 41
 
         puts "\n1. What is your Team Name (e.g. teamname.hipchat.com)"
         print "> "
-        teamname = parse_user_input(gets.strip)
-        puts "\n2. We need a HipChat token (visit https://#{teamname}.hipchat.com/account/api)"
+        team = parse_user_input(gets.strip)
+        puts "\n2. We need a HipChat token (visit https://#{team}.hipchat.com/account/api)"
         puts "(ensure the scope 'Send Message' is selected)"
         print "> "
         token = parse_user_input(gets.strip)
@@ -123,9 +102,9 @@ module Lolcommits
         room = parse_user_input(gets.strip)
 
         {
-          'api_team'  => teamname,
-          'api_token' => token,
-          'api_room'  => room
+          api_team: team,
+          api_token: token,
+          api_room: room
         }
       end
 
@@ -156,7 +135,7 @@ module Lolcommits
       end
 
       def api_url
-        URI("http://#{configuration['api_team']}.hipchat.com/v2/room/#{configuration['api_room']}/share/file?auth_token=#{configuration['api_token']}")
+        URI("http://#{configuration[:api_team]}.hipchat.com/v2/room/#{configuration[:api_room]}/share/file?auth_token=#{configuration[:api_token]}")
       end
 
       def message
--- lolcommits-hipchat.gemspec
+++ lolcommits-hipchat.gemspec
@@ -36,7 +36,7 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = ">= 2.0.0"
   spec.add_runtime_dependency "mime-types"
 
-  spec.add_development_dependency "lolcommits", ">= 0.9.5"
+  spec.add_development_dependency "lolcommits", ">= 0.10.0"
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "webmock"
   spec.add_development_dependency "pry"
