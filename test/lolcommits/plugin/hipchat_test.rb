d202d2c4b1e3a1b07ddcf0095cc90e1454063557
Matthew Hutchinson <matt@hiddenloop.com>
2018-02-05 21:25:50 +0000

    drop Ruby 2.0 support, bump gem version

--- test/lolcommits/plugin/hipchat_test.rb
+++ test/lolcommits/plugin/hipchat_test.rb
@@ -40,7 +40,7 @@ describe Lolcommits::Plugin::Hipchat do
 
     describe "#enabled?" do
       it "is disabled by default" do
-        assert_nil plugin.enabled?
+        plugin.enabled?.must_equal false
       end
 
       it "is true when configured" do
